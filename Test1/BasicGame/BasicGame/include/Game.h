e0c2c2bd379321ccf337943970dbe0c3f1251a7c
Jason <jason.bunn009@gmail.com>
2015-02-18 22:03:20 -0500

    Starting Breakout style game structure

--- Test1/BasicGame/BasicGame/include/Game.h
+++ Test1/BasicGame/BasicGame/include/Game.h
@@ -1,9 +1,13 @@
 #ifndef GAME_H
 #define GAME_H
+#include <SDL.h>
+#include "Ball.h"
 
 class Game{
 private:
 	bool			exitGame;
+	SDL_Window		*window;
+	SDL_Renderer	*renderer;
 public:
 					Game();
 					~Game();
@@ -12,9 +16,11 @@ public:
 	void			Run();
 	void			Update();
 	void			Render();
-	void			ProcessEvents();
+	void			ProcessEvents(const SDL_Event &e);
 	void			ProcessInput();
 	void			CleanupResources();
+
+	void			LogSDLError(std::ostream &os, const std::string &msg);
 };
 
 #endif
--- Test1/BasicGame/BasicGame/src/Game.cpp
+++ Test1/BasicGame/BasicGame/src/Game.cpp
@@ -1,7 +1,8 @@
-#include <SDL.h>
-#include <iostream>
-#include "Game.h"
 
+#include <iostream>
+#include <string>
+#include "Game.h"
+#include "Cleanup.h"
 
 
 
@@ -19,19 +20,46 @@ bool Game::Init()
 {
 	bool success = true;
 	//Initialize SDL Resources
-
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		LogSDLError(std::cout, "SDL_Init");
+		return false;
+	}
 	
+	window = SDL_CreateWindow("Basic Game tests", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+	{
+		LogSDLError(std::cout, "CreateWindow");
+		SDL_Quit();
+		return false;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr)
+	{
+		LogSDLError(std::cout, "CreateRenderer");
+		cleanup(window);
+		SDL_Quit();
+		return false;
+	}
+
 	return success;
 }
 
 void Game::Run()
 {
 	exitGame = false;
+	SDL_Event e;
 	//Main game loop
 	while (!exitGame)
 	{
-		//debug only
-		exitGame = true;
+		//process events
+		while (SDL_PollEvent(&e))
+		{
+			ProcessEvents(e);
+		}
+		
+		
 	}
 	std::cout << "Exit condition reached" << std::endl;
 	SDL_Delay(2000);
@@ -39,9 +67,29 @@ void Game::Run()
 	SDL_Quit();
 }
 
-void Game::ProcessEvents()
+void Game::ProcessEvents(const SDL_Event &e)
 {
 	//Process system related events
+	if (e.type == SDL_KEYDOWN)
+	{
+		switch (e.key.keysym.sym)
+		{
+		case SDLK_ESCAPE:
+			std::cout << "Key press detected" << std::endl;
+			exitGame = true;
+			break;
+
+		default:
+			std::cout << "Key press detected" << std::endl;
+			exitGame = true;
+			break;
+		}
+
+	}
+	if (e.type == SDL_QUIT)
+	{
+		exitGame = true;
+	}
 }
 
 void Game::ProcessInput()
@@ -62,4 +110,9 @@ void Game::Update()
 void Game::CleanupResources()
 {
 	//Cleanup all SDL resources used
+}
+
+void Game::LogSDLError(std::ostream &os, const std::string &msg)
+{
+	os << msg << " error: " << SDL_GetError() << std::endl;
 }
