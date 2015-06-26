388e1ee0ba1301946bed056db6a02d08e1abaa46
jason-bunn <jason.bunn009@gmail.com>
2015-06-25 23:10:19 -0400

    Chapter 5 bugfix

--- SDL-Game-Dev/Ch-5/Chapter5/Chapter5/AnimatedGraphic.cpp
+++ SDL-Game-Dev/Ch-5/Chapter5/Chapter5/AnimatedGraphic.cpp
@@ -2,10 +2,22 @@
 
 AnimatedGraphic::AnimatedGraphic(const LoaderParams* pParams, int animSpeed) : SDLGameObject(pParams), m_animSpeed(animSpeed)
 {
-
+	m_numFrames = 2;
 }
 
 void AnimatedGraphic::update()
 {
 	m_currentFrame = int(((SDL_GetTicks() / (1000 / m_animSpeed)) % m_numFrames));
+
+	SDLGameObject::update();
+}
+
+void AnimatedGraphic::draw()
+{
+	SDLGameObject::draw();
+}
+
+void AnimatedGraphic::clean()
+{
+
 }
--- SDL-Game-Dev/Ch-5/Chapter5/Chapter5/AnimatedGraphic.h
+++ SDL-Game-Dev/Ch-5/Chapter5/Chapter5/AnimatedGraphic.h
@@ -10,6 +10,9 @@ public:
 	AnimatedGraphic(const LoaderParams* pParams, int animSpeed);
 
 	virtual void update();
+	virtual void draw();
+	virtual void clean();
+
 private:
 
 	int m_animSpeed;
